e75ef66d419815b981e39ad519b1b4b70f851b08
austinricha15487 <34347111+austinricha15487@users.noreply.github.com>
2018-05-03 09:25:25 -0500

    update to main

--- Chapter-11-master/Advanced32 Project-IM/Advanced32.cpp
+++ Chapter-11-master/Advanced32 Project-IM/Advanced32.cpp
@@ -8,36 +8,42 @@ using namespace std;
 int main()
 {
 	//declare variables
-	int sub    = 0;    //keeps track of subscripts
-	int points = 0;  
-	int pointsPossible = 0;
-
-	//get total possible points
-	cout << "Enter the number of possible points: ";
-	cin >> pointsPossible;
-
+	int sub = 0;    //keeps track of subscripts
+	int mostPossiblePoints = 0;
+	int pointsEarned;
+	char finalGrade;
 	//declare arrays and fill with data
-	
-	
+	int totalPoints[] = { 0, 300, 350, 400, 450 };
+	char grade[] = { 'F', 'D', 'C', 'B', 'A' };
 	//get total points
-	cout << "Enter points earned (negative number to stop program): ";
-	cin >> points;
-	
-	while (points >= 0)
+	cout << "Enter most points possible (negative number to stop program): ";
+	cin >> mostPossiblePoints;
+	cout << "Please enter how many points this student scored on the test!" << endl;
+	cin >> pointsEarned;
+	if (pointsEarned >= (mostPossiblePoints * .9))
 	{
-		//search for points in minPoints array
-		sub = 0;
-		while (sub < 5 && minPoints[sub] <= points)
-			sub += 1;
-		//end while
-
-		//display grade from grade array
-		cout << "Grade: " << grade[sub - 1] << endl << endl;
-		
-		cout << "Enter points earned (negative number to stop program): ";
-		cin >> points;
-	} //end while
-
-	//system("pause");
+		cout << "This student recieved a grade of: " << grade[4] << endl;
+	}
+	else if (pointsEarned >= (mostPossiblePoints * .8) && pointsEarned < (mostPossiblePoints * .9))
+	{
+		cout << "This student recieved a grade of: " << grade[3] << endl;
+	}
+	else if (pointsEarned >= (mostPossiblePoints * .7) && pointsEarned < (mostPossiblePoints * .8))
+	{
+		cout << "This student got a grade of: " << grade[2] << endl;
+	}
+	else if (pointsEarned >= (mostPossiblePoints * .6) && pointsEarned < (mostPossiblePoints *.7))
+	{
+		cout << "This student recieved a grade of: " << grade[1] << endl;
+	}
+	else if (pointsEarned < (mostPossiblePoints * .6) && pointsEarned > 0)
+	{
+		cout << "This student sucks and recieved an " << grade[0];
+	}
+	else if (pointsEarned < 0)
+	{
+		return 0;
+	}
+	system("pause");
 	return 0;
 }	//end of main function
